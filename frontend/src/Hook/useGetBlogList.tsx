dabaaa4256804c6b6dd01e43d9355fb01b6e1e1d
Dorian Canonne <dorian.canonne@novanexx.com>
2022-04-29 16:29:20 +0200

    final

--- frontend/src/Hook/useGetBlogList.tsx
+++
@@ -1,8 +0,0 @@
-import {BlogInterface} from "../Interface/ResponsesInterfaces";
-
-export default function useGetBlogList() {
-    return (): Promise<BlogInterface[]> => {
-        return fetch('http://localhost:2345')
-            .then(res => res.json())
-    }
-}
